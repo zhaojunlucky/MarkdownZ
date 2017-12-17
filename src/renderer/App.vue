7b8a02a00a9f9eba522c2015f4ab9e19af46d655
magicworldz <zhaojunlucky@qq.com>
2017-12-17 16:40:23 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'markdownz'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
